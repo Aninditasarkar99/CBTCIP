066a14c29a71bab4d847467de47d75d63ceca8cb
Anindita Sarkar <sarkaranindita55@gmail.com>
2023-07-30 01:29:41 +0530

    3rd Internship

---
+++ Calculator/calculator.js
@@ -0,0 +1,20 @@
+ function show(data){
+    let v = document.getElementById('show');
+    v.value += data;
+    
+ }
+ function Result() {
+    var number1 = document.getElementById('show').value;
+    var number2 = eval(number1);
+    document.getElementById('show').value = number2;
+ }
+
+ function Clear() {
+    var inp = document.getElementById('show');
+    inp.value = '';
+ }
+
+ function Backspace() {
+    var del = document.getElementById('show');
+    del.value = del.value.slice(0,-1);
+ }
